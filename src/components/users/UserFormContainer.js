56841a2c909863c7af4617ff3711a1fac5cfd418
Gerardo Fernández <gerardo@latteandcode.com>
2019-05-23 17:17:54 +0200

    refactor userformcontainer

--- src/components/users/UserFormContainer.js
+++ src/components/users/UserFormContainer.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Alert from 'Components/styled/Alert';
 import userPropType from 'PropTypes/userPropType';
+import UserForm from 'Components/users/UserForm';
 
 export default class UserFormContainer extends PureComponent {
   static propTypes = {
@@ -15,32 +15,22 @@ export default class UserFormContainer extends PureComponent {
 
   state = {
     // eslint-disable-next-line react/destructuring-assignment
-    userModel: this.props.user || { first_name: '', email: '' },
+    userModel: this.props.user
+      ? // eslint-disable-next-line react/destructuring-assignment
+        { ...this.props.user }
+      : { first_name: '', email: '' },
     sending: false,
-    error: null
+    error: null,
+    success: false
   };
 
-  onSubmit = async event => {
+  onSubmitForm = async event => {
     event.preventDefault();
     const { onSubmit } = this.props;
     const { userModel } = this.state;
     this.setState({ sending: true });
     await onSubmit(userModel);
     this.setState({ sending: false, error: null, success: true });
-  };
-
-  renderError = () => {
-    const { error } = this.state;
-    return error ? (
-      <Alert error>
-        {error.code === 404 ? 'No se encontró el usuario' : error.message}
-      </Alert>
-    ) : null;
-  };
-
-  renderSuccess = () => {
-    const { success } = this.state;
-    return success ? <Alert success>Formulario enviado con éxito</Alert> : null;
   };
 
   onChangeName = event => {
@@ -58,40 +48,17 @@ export default class UserFormContainer extends PureComponent {
   };
 
   render() {
-    const { sending, userModel } = this.state;
+    const { sending, success, error, userModel } = this.state;
     return (
-      <>
-        {this.renderError()}
-        {this.renderSuccess()}
-        <form onSubmit={this.onSubmit}>
-          <div className="form-group">
-            <label htmlFor="first_name">Nombre</label>
-            <input
-              id="first_name"
-              type="text"
-              name="first_name"
-              className="form-control"
-              onChange={this.onChangeName}
-              value={userModel.first_name}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="text"
-              name="email"
-              className="form-control"
-              onChange={this.onChangeEmail}
-              value={userModel.email}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Enviar
-          </button>
-        </form>
-        {sending ? <div>Enviando</div> : null}
-      </>
+      <UserForm
+        success={success}
+        error={error}
+        sending={sending}
+        user={userModel}
+        onChangeEmail={this.onChangeEmail}
+        onChangeName={this.onChangeName}
+        onSubmit={this.onSubmitForm}
+      />
     );
   }
 }
--- src/prop-types/userPropType.js
+++ src/prop-types/userPropType.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 
 export default PropTypes.shape({
-  id: PropTypes.number.isRequired,
+  id: PropTypes.number,
   first_name: PropTypes.string.isRequired,
-  last_name: PropTypes.string.isRequired,
+  last_name: PropTypes.string,
   email: PropTypes.string.isRequired
 });
